8e7ea3d8c79b062ba0e2223292ddc3e3565d4619
antoninkoren <antonin.koren.s@gyarab.cz>
2025-12-01 12:01:36 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const cacheName = 'obqr-cache-v1';
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  'https://cdn.jsdelivr.net/npm/jsqr/dist/jsQR.js'
+];
+
+self.addEventListener('install', e=>{
+    e.waitUntil(
+        caches.open(cacheName).then(cache=>{
+            return cache.addAll(assets);
+        })
+    );
+});
+
+self.addEventListener('fetch', e=>{
+    e.respondWith(
+        caches.match(e.request).then(response=>{
+            return response || fetch(e.request);
+        })
+    );
+});
